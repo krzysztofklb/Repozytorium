2c5e319844ef86eb74cae88fed7e506fd8c59e29
erwin <erwin@example.pl>
2016-01-30 20:09:05 +0100

    commit o nazwie jeden

--- kosztpaliwa.c
+++ kosztpaliwa.c
@@ -1,11 +1,15 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 
 int main() {
 
 float paliwo,trasa,spalanie;
-paliwo=3.77;
-spalanie=6.2;
-trasa=45.7;
+printf ("Podaj cene paliwa");
+scanf(paliwo);
+
+printf ("Podaj spalanie");
+scanf (spalanie);
+printf ("Podaj trase");
+scanf(trasa);
 printf("Za paliwo zapłacisz: %.2f zł", (((trasa*spalanie)/100)*paliwo));
 	return 0;
 }
