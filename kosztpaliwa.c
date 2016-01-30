f9f1a5c5f2a8d9ed1114a16f69e68f120e6099f8
krzysztofklb <krzysztofklb@gmail.com>
2016-01-30 19:32:03 +0100

    Create kosztpaliwa.c

---
+++ kosztpaliwa.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+float paliwo,trasa,spalanie;
+paliwo=3.77;
+spalanie=6.2;
+trasa=45.7;
+printf("Za paliwo zapłacisz: %.2f zł", (((trasa*spalanie)/100)*paliwo));
+	return 0;
+}
